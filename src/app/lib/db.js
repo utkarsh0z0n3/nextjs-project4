7109b22d282a0cf05189fec737016c28658dd754
utkarsh0z0n3 <113238570+utkarsh0z0n3@users.noreply.github.com>
2024-04-18 19:58:20 +0530

    Update db.js

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,2 +1,2 @@
 const {username,password} = process.env
-export const connectionstr ="mongodb+srv://utksaxena44:"+password+"@cluster0.kuirkfg.mongodb.net/restoDB?retryWrites=true&w=majority&appName=Cluster0";
+export const connectionstr ="mongodb+srv://utksaxena44:Insta2024@cluster0.kuirkfg.mongodb.net/restoDB?retryWrites=true&w=majority&appName=Cluster0";
